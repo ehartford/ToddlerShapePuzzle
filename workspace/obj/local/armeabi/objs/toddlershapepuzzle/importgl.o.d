8f34ee6e40f5d0063855f0c58220a71d7326348a
Eric Stob <ericstob@gmail.com>
2011-10-26 21:51:03 -0700

    initial code

---
+++ workspace/obj/local/armeabi/objs/toddlershapepuzzle/importgl.o.d
@@ -0,0 +1,23 @@
+/cygdrive/c/ToddlerShapePuzzle/workspace/obj/local/armeabi/objs/toddlershapepuzzle/importgl.o: \
+ /cygdrive/c/ToddlerShapePuzzle/workspace/jni/importgl.c \
+ /cygdrive/c/ToddlerShapePuzzle/workspace/jni/importgl.h \
+ /cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/GLES/gl.h \
+ /cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h \
+ /cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h \
+ /cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/stdint.h \
+ /cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/machine/_types.h
+
+/cygdrive/c/ToddlerShapePuzzle/workspace/jni/importgl.h:
+
+/cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/GLES/gl.h:
+
+/cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h:
+
+/cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h:
+
+/cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/stdint.h:
+
+/cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/c/Android/android-ndk-r6b/platforms/android-4/arch-arm/usr/include/machine/_types.h:
